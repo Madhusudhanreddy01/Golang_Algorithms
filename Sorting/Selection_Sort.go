034a8386f1bdc215eda93a3ab3ee63fa2c392aaf
Madhusudhanreddy <madhusudhan.pannala01@gmail.com>
2022-11-30 13:21:19 +0530

    First commit

---
+++ Sorting/Selection_Sort.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func selectionSort(A []int) []int {
+	var n = len(A)
+	for i := 0; i < n; i++ {
+		var minIndex = i
+		for j := i; j < n; j++ {
+			if A[j] < A[minIndex] {
+				minIndex = j
+			}
+		}
+		A[i], A[minIndex] = A[minIndex], A[i]
+	}
+	return A
+}
+
+func main() {
+	A := []int{3, 4, 5, 2, 1}
+	fmt.Println(selectionSort(A))
+	// A = selectionSort(A)
+	// fmt.Println("\n After Selection Sorting")
+	// for _, val := range A {
+	// 	fmt.Println(val)
+	// }
+}
